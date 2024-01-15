0a6d23373595e241839abde88c48a83bb4e56eb5
serdem21 <ayducity1@gmail.com>
2024-01-15 13:16:42 -0800

    AQA-003 Updating locater

---
+++ src/test/java/Methods_Arrays/ForEchLoop.java
@@ -0,0 +1,29 @@
+package Methods_Arrays;
+
+public class ForEchLoop {
+
+    public static void main(String[] args) {
+
+        int[]arr ={4,5,12,6};
+
+        for(int i =0; i<arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
+            System.out.println("-----------------------");
+
+            for(int each : arr){
+                System.out.println(each);
+            }
+
+            /*
+
+            for(DATATYPE NAME : DATA_STRUCTURE){
+                CODE
+
+
+
+             */
+        }
+    }
+
